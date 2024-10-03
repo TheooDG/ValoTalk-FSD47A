8c7ab5a169078ec9e6e21327beddc4bbc7e913ff
Théo <theodgt5@gmail.com>
2024-10-03 19:19:54 +0200

    Update de User.php et nouvelle migration

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -3,28 +3,53 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Validator\Constraints\Email;
 
 #[ORM\Entity]
 #[ORM\Table('user')]
-class User extends AbstractEntity
+class User extends AbstractEntity implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
     #[ORM\GeneratedValue(strategy: 'IDENTITY')]
     #[ORM\Column('id', type: 'bigint')]
     protected $id;
 
-    #[ORM\Column('email')]
+    #[ORM\Column('username', type: 'string')]
+    private string $username;
+
+    #[ORM\Column('email', type: 'string')]
     #[Email]
     private string $email;
+
+    #[ORM\Column('password', type: 'string')]
+    private string $password;
+
+    #[ORM\Column('roles', type: 'json')]
+    private array $roles = [];
 
     #[ORM\Column('birthdate', type: 'datetime')]
     private ?\DateTime $birthdate;
 
-    public function __construct(string $email, ?\DateTime $birthdate)
+    public function __construct(string $username, string $email, string $password, ?\DateTime $birthdate)
     {
+        $this->username  = $username;
         $this->email     = $email;
+        $this->password  = $password;
         $this->birthdate = $birthdate;
+    }
+
+    public function getUsername(): string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(string $username): self
+    {
+        $this->username = $username;
+
+        return $this;
     }
 
     public function getEmail(): string
@@ -39,6 +64,33 @@ class User extends AbstractEntity
         return $this;
     }
 
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password;
+
+        return $this;
+    }
+
+    public function getRoles(): array
+    {
+        $roles   = $this->roles;
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    public function setRoles(array $roles): self
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
     public function getBirthdate(): ?\DateTime
     {
         return $this->birthdate;
@@ -49,5 +101,15 @@ class User extends AbstractEntity
         $this->birthdate = $birthdate;
 
         return $this;
+    }
+
+    public function eraseCredentials(): void
+    {
+        // supprime les données sensibles ne devant pas être conservées trop longtemps. Format : $this->var = null;
+    }
+
+    public function getUserIdentifier(): string
+    {
+        return $this->email;
     }
 }
